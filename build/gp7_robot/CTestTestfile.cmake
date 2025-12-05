c0a071733c63adcd83f697872241d47619495054
PankajaBalasooriya <pankajabalasooriya566@gmail.com>
2025-12-05 17:11:14 +1100

    initial

---
+++ build/gp7_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pankaja/Downloads/GP7_Robot.SLDASM
+# Build directory: /home/pankaja/Downloads/GP7_Robot.SLDASM/build/gp7_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
